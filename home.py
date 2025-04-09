1dc5d930233b9ae84b611bc1319adccc099c261f
Martina Garcia <martinagg@myuax.com>
2025-04-09 14:38:34 +0200

    cr: start app

---
+++ home.py
@@ -0,0 +1,7 @@
+# Home.py
+import streamlit as st
+
+st.set_page_config(page_title="Greenergy", page_icon="🌱", layout="wide")
+
+st.title("🌱 Greenergy - Centro de Control")
+st.write("Bienvenido al sistema inteligente de gestión de placas solares.")
